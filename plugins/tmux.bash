11beb59833be8c5f9ab27664c3722999bb40c771
Guillaume Frichet <guillaume.frichet@gmail.com>
2017-03-02 12:02:26 +0100

    +

--- plugins/tmux.bash
+++ plugins/tmux.bash
@@ -9,6 +9,31 @@ t() {
   fi
 }
 
+# tmux_session_exists() {
+#   local session_name="$1"
+#   tmux list-sessions | sed -E 's/:.*$//' | grep -q "^$session_name$"
+# }
+
+# tmux_create_detached_session() {
+#   local session_name="$1"
+#   (TMUX='' tmux new-session -Ad -s "$session_name")
+# }
+
+# Attach or create tmux session named the same as current directory
+# https://github.com/thoughtbot/dotfiles/blob/master/bin/tat
+# tmux_create_if_needed_and_attach() {
+#   local path_name="$(basename "$PWD" | tr . -)"
+#   local session_name="${1:-$path_name}"
+#   if [[ -z "$TMUX" ]]
+#   then tmux new-session -As "$session_name"
+#   else
+#     if ! tmux_session_exists "$session_name"
+#     then tmux_create_detached_session "$session_name"
+#     fi
+#     tmux switch-client -t "$session_name"
+#   fi
+# }
+
 if hash _tmux 2>/dev/null
 then complete -F _tmux t
 fi
