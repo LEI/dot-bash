a235f4073be60e7dd86e7b161cab52d0f14f3bf9
Guillaume Frichet <guillaume.frichet@gmail.com>
2018-03-06 12:06:10 +0100

    dotrc

--- plugins/rbenv.bash
+++ plugins/rbenv.bash
@@ -6,7 +6,7 @@ fi
 
 export RBENV_SHELL=bash
 export RBENV_SHIMS="$HOME/.rbenv/shims"
-pathmunge "$RBENV_SHIMS"
+pathmunge "$RBENV_SHIMS" before
 RBENV_BASH_COMPLETION="$(brew --cellar)/rbenv/$(rbenv --version | cut -d ' ' -f2)/completions/rbenv.bash"
 source "$RBENV_BASH_COMPLETION"
 command rbenv rehash 2>/dev/null
