d6412cbffc49c0c8d26c06a67ebb40f1b12cf33c
Guillaume Frichet <guillaume.frichet@gmail.com>
2018-07-15 12:40:35 +0200

    gist

--- plugins/git.bash
+++ plugins/git.bash
@@ -18,3 +18,17 @@ g() {
 if hash _git 2>/dev/null; then
   complete -o bashdefault -o default -o nospace -F _git g # __git_wrap__git_main
 fi
+
+if ! hash gist 2>/dev/null || [[ "$_custom_gist" -eq 1 ]]; then
+  gist() {
+    local cmd="$1"
+    shift
+    local base_dir="$HOME/src/gist.github.com"
+    case "$cmd" in
+      '') find "$base_dir" -mindepth 2 -maxdepth 2 -type d ;; # List
+      clone) git clone "$@" ;;
+      # add) git clone "$1" "${2:-$base_dir/...}" ;;
+    esac
+  }
+  _custom_gist=1
+fi
