7092d3bbf883f924c04da5f7c63b44b1246de6a1
LEI <guillaume.frichet@gmail.com>
2018-07-30 21:49:41 +0200

    c

--- plugins/fzf.bash
+++ plugins/fzf.bash
@@ -23,11 +23,12 @@ else
         sed s/^..//) 2> /dev/null'
 fi
 
-# export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-# export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_DEFAULT_OPTS='' # --height 40% --reverse --border
 # --color fg:242,bg:236,hl:65,fg+:15,bg+:239,hl+:108
 # --color info:108,prompt:109,spinner:108,pointer:168,marker:168
+
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # https://github.com/junegunn/fzf/wiki/Color-schemes
 _gen_fzf_default_opts() {
