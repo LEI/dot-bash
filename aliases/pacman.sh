dd6696d102b018d2db4c3a2bb0943ba2bec817b0
LEI <guillaume.frichet@gmail.com>
2018-07-31 08:13:36 +0200

    +

---
+++ aliases/pacman.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if ! hash pacman 2>/dev/null; then
+  return
+fi
+
+# alias syu="pb -Syu && pacman -Fy"
+# alias pacs="pb -S"
+# alias pacss="pb -Ss"
+# alias pacr="pacman -Rcs"
+# alias pacwhat="pacman -Qi"
+# alias pacwhich="pacman -Qo"
+# alias paclist="pacman -Ql"
+# alias pacwhere="pacman -Fs"
+# alias pacdead="pacman -Qdt"
