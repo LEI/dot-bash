323265988ef2700d765e02dae2c9b3d5b3e6742e
LEI <guillaume.frichet@gmail.com>
2018-09-01 14:00:30 +0200

    npm

--- plugins/npm.bash
+++ plugins/npm.bash
@@ -4,8 +4,13 @@ if ! hash npm 2>/dev/null; then
   return
 fi
 
+# https://docs.npmjs.com/getting-started/fixing-npm-permissions
+
+# https://wiki.archlinux.org/index.php/Node.js#Allow_user-wide_installations
+export npm_config_prefix=~/.node_modules # "$HOME/.node_modules"
+
 # if [[ -z "${NODE_MODULES_PATH-}" ]]; then
 #   NODE_MODULES_PATH="$(npm config get prefix)"
 # fi
-# pathmunge "$NODE_MODULES_PATH" after
-pathmunge "$HOME/.node_modules/bin" after
+
+pathmunge "$npm_config_prefix/bin" after
