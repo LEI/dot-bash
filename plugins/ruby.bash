3e713b4c8e3c2ebf5330c648be98cb8757558ff9
LEI <guillaume.frichet@gmail.com>
2018-09-01 16:31:59 +0200

    gemhome

--- plugins/ruby.bash
+++ plugins/ruby.bash
@@ -4,6 +4,19 @@ if ! hash ruby 2>/dev/null; then
   return
 fi
 
-export GEM_HOME="$(ruby -e 'print Gem.user_dir')"
-# GEM_PATH, BUNDLE_PATH
+# gemdir="$(gem env gemdir)"
+# gempath="$(gem env gempath)"
+
+# gemdir: $HOME/.gem/ruby/X.X.X
+userdir="$(ruby -e 'print Gem.user_dir')"
+
+export GEM_HOME="$userdir"
+# export GEM_PATH="$gempath:$GEM_HOME"
+# OSX GEM PATHS:
+#   - $HOME/.gem/ruby/2.5.0
+#   - /usr/local/lib/ruby/gems/2.5.0
+#   - /usr/local/Cellar/ruby/2.5.1/lib/ruby/gems/2.5.0
+
+# BUNDLE_PATH
+
 pathmunge "$GEM_HOME/bin" after
